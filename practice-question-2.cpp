dd0a28f2592adca91b7eeca65032b69f1d88cd60
mifrajawed <mifraaa.27@gmail.com>
2026-09-07 08:48:01 -0700

    practice-question-2.cpp

---
+++ practice-question-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 
+ //program to find the greater number.
+ 
+    int a , b ;
+    cout<<"Enter the first number"<<"\n";
+    cin>>a;
+    
+    cout<<"Enter the second number"<<"\n";
+    cin>>b;
+    
+    if(a>b)
+    {
+       cout<<"a is greater"<<"\n";
+    }
+      
+    else{
+        cout<< " b is greater"<<"\n";
+       
+      }
+       
+    return 0;
+}
