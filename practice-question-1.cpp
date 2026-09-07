129147a2fe3af013c3800e5030caa0c59cea3bc7
mifrajawed <mifraaa.27@gmail.com>
2026-09-07 08:16:50 -0700

    Create practice-question-1.cpp

---
+++ practice-question-1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int c= 53 ;
+
+int main()
+{
+int a ,b ;
+cout<<" Enter the vlue of a " <<"\n";
+cin>>a;
+
+cout<<"Enter the value of b"  <<"\n";
+cin>>b;
+
+cout<<"sum of a+b is "<<a+b<<"\n";
+cout<<"difference of a-b is "<<a-b<<"\n";
+cout<<" Product of a*b is "<<a*b<<"\n";
+
+    return 0;
+}
