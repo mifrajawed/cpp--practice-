99864d0963cc6bdebffe0fbee111a0771ea105c6
mifrajawed <mifraaa.27@gmail.com>
2026-09-13 11:42:23 -0700

    Create practice-question-3.cpp

---
+++ practice-question-3.cpp
@@ -0,0 +1,28 @@
+include <iostream>
+
+using namespace std;
+int main()
+{ 
+    //program to check even and odd number between two numbers:
+    
+  int a,b;
+    cout<<" The value of a is"<<"\n";
+    cin>>a;
+     
+    cout<<" The value of a is"<<"\n";
+    cin>>b; 
+    
+ if(a % 2 ==0){
+     cout <<"a is even number"<<"\n";
+ }
+ else{
+     cout<<"a is odd number"<<"\n";
+ }
+ if(b % 2 ==0){
+     cout <<"b is even number"<<"\n";
+ }
+ else{
+     cout<<"b is odd number"<<"\n";
+ } 
+    return 0;
+}
